16aac2672ff85c374150fe954204798d8dd9fc03
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-12 17:59:15 +0530

    connected firebase

--- app/src/main/java/com/example/hungerspot/MainActivity.kt
+++ app/src/main/java/com/example/hungerspot/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.hungerspot
 
-import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +19,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val reffs=FirebaseDatabase.getInstance().reference.child("name");
+        val obj:donorclass?=null;
+        obj?.setname("dsf");
+        obj?.setplace("df")
+
+//        reffs.addValueEventListener(object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                val value =
+//                    dataSnapshot.getValue(String::class.java)!!
+////                Toast.makeText(this,value,Toast.LENGTH_SHORT).show();
+//
+//                Log.d("check it", "Value is: $value")
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                // Failed to read value
+////                Log.w(FragmentActivity.TAG, "Failed to read value.", error.toException())
+//            }
+//        })
 
 
-        val Intent=Intent(this,DonorLoginActivity::class.java);
-        startActivity(Intent);
+//        reffs.push().setValue(obj);
+
+//        val Intent=Intent(this,DonorLoginActivity::class.java);
+//        startActivity(Intent);
+        // Write a message to the database
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/hungerspot/donorclass.kt
@@ -0,0 +1,15 @@
+package com.example.hungerspot
+
+public class donorclass {
+    var name: String? =null;
+    var place:String?=null;
+
+    fun setname(names:String){
+        name=names;
+    }
+    fun setplace(places:String){
+        place=places;
+    }
+
+
+}
