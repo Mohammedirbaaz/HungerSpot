9dd8cd0b8f9056b8ad641a97efef2344e72219cd
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-12 12:51:50 +0530

    added basic activities

---
+++ app/src/main/java/com/example/hungerspot/DonorRequestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hungerspot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonorRequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donor_request)
+    }
+}
---
+++ app/src/main/java/com/example/hungerspot/DonorVolunteerBioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hungerspot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonorVolunteerBioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donor_volunteer_bio)
+    }
+}
