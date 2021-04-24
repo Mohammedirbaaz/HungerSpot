22e73a5b551fb0d55bc536db8c9dfdbfe6ed2715
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-24 20:21:42 +0530

    navigation drawer and account details can be viewed

--- app/src/main/java/com/example/hungerspot/DonorMainActivity.kt
+++ app/src/main/java/com/example/hungerspot/DonorMainActivity.kt
@@ -1,32 +1,76 @@
 package com.example.hungerspot
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.app.Notification
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
-import android.widget.Button
+import android.util.AttributeSet
+import android.view.Menu
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.w3c.dom.Text
 
 class DonorMainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_donor_main);
-
-        var logoutbtn=findViewById<Button>(R.id.logoutbtnid);
-        var textviews=findViewById<TextView>(R.id.sessioidtext);
-        var  sg= SessionManagment();
-        sg.SessionManagement2(this@DonorMainActivity);
-
-        textviews.text=sg.getSession();
-
-        logoutbtn.setOnClickListener {
-            sg.removeSession();
-            movetopublicmainactivity();
+        setContentView(R.layout.activity_donor_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
 
-        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_mycontribution,R.id.nav_mydetail), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController);
+
+
     }
-    fun movetopublicmainactivity(){
-        val intent: Intent = Intent(this,MainActivity::class.java);
-        startActivity(intent);
+    override fun onBackPressed(){
+
+        var alert=AlertDialog.Builder(this);
+
+        alert.setMessage("Are you sure? you want to exit?").setPositiveButton("Yes"){dialog: DialogInterface?, which: Int ->
+            finish()
+        }.setNegativeButton("Stay"){dialog:DialogInterface, which -> }
+        val alertDialog: AlertDialog = alert.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.donor_main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
 }
---
+++ app/src/main/java/com/example/hungerspot/Mydetails.kt
@@ -0,0 +1,110 @@
+package com.example.hungerspot
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.text.Editable
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import org.w3c.dom.Text
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Mydetails.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Mydetails : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mydetails, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var namess=view.findViewById<TextView>(R.id.idname);
+        var emailss=view.findViewById<EditText>(R.id.idemail);
+        var addresss=view.findViewById<EditText>(R.id.idaddress);
+        var landmarks=view.findViewById<EditText>(R.id.idlandmark);
+        var phnoss=view.findViewById<EditText>(R.id.idphno);
+        var pincodess=view.findViewById<EditText>(R.id.idpincode);
+
+
+        val sessionManagement = SessionManagment();
+        activity?.let { sessionManagement.SessionManagement2(it) };
+        var useridsss=sessionManagement.getSession();
+        Toast.makeText(activity,useridsss,Toast.LENGTH_SHORT).show();
+        var reffs=FirebaseDatabase.getInstance().getReference("Donor").child(useridsss.toString());
+        reffs.addValueEventListener(object :ValueEventListener{
+            override fun onCancelled(error: DatabaseError) { }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(h in snapshot.children){
+                    if(h.key.toString()=="name"){
+                        namess.text=h.value.toString();
+                    }
+                    else if(h.key.toString()=="email"){
+                        emailss.text=Editable.Factory.getInstance().newEditable(h.value.toString());
+                    }
+                    else if(h.key.toString()=="address"){
+                        addresss.text=Editable.Factory.getInstance().newEditable(h.value.toString());
+                    }
+                    else if(h.key.toString()=="landmark"){
+                        landmarks.text=Editable.Factory.getInstance().newEditable(h.value.toString());
+                    }
+                    else if(h.key.toString()=="phno"){
+                        phnoss.text=Editable.Factory.getInstance().newEditable(h.value.toString());
+                    }
+                    else if(h.key.toString()=="pincode"){
+                        pincodess.text=Editable.Factory.getInstance().newEditable(h.value.toString());
+                    }
+
+                }
+            }
+
+        })
+
+    }
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Mydetails().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
