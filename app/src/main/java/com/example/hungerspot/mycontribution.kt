b69d8bdcd8bfbf42604c506f16a189795878eea8
Mohammedirbaaz <58600867+Mohammedirbaaz@users.noreply.github.com>
2021-04-27 01:07:13 +0530

    started working on recyclerview for mycontribution.kt

--- app/src/main/java/com/example/hungerspot/mycontribution.kt
+++ app/src/main/java/com/example/hungerspot/mycontribution.kt
@@ -1,11 +1,24 @@
 package com.example.hungerspot
 
+import android.accounts.AccountManager.get
+import android.media.Image
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.squareup.picasso.Picasso
+import java.lang.reflect.Array.get
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +29,8 @@ class mycontribution : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var mRecyclerview:RecyclerView
+    lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +50,62 @@ class mycontribution : Fragment() {
         return inflater.inflate(R.layout.fragment_mycontribution, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sessionManagement = SessionManagment();
+        activity?.let { sessionManagement.SessionManagement2(it) };
+        var useridsss=sessionManagement.getSession();
+        val ff="||";
+        val list= useridsss?.split(ff);
+        val userid= list?.get(0);
+        val pincode= list?.get(1);
+
+        mRecyclerview=view.findViewById(R.id.idlistview);
+        Log.i("yessss",pincode+" "+userid);
+        mDatabase=FirebaseDatabase.getInstance().getReference("Donor").child("mbappe||142536").child("MyContribution");
+        mDatabase.keepSynced(true);
+        mRecyclerview.setHasFixedSize(true);
+        mRecyclerview.layoutManager=LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+        logicRecyclerView();
+
+    }
+    fun logicRecyclerView(){
+
+
+
+        val firebaseRecyclerOptions=FirebaseRecyclerOptions.Builder<Contribution>()
+            .setQuery(mDatabase,Contribution::class.java)
+            .build();
+
+
+        val firebaseRecyclerAdapter=object : FirebaseRecyclerAdapter<Contribution,contributeViewHolder>(firebaseRecyclerOptions){
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): contributeViewHolder {
+                val v:View=LayoutInflater.from(parent.context).inflate(R.layout.list_layout,parent,false)
+                return contributeViewHolder(v)
+            }
+
+            override fun onBindViewHolder(
+                holder: contributeViewHolder,
+                position: Int,
+                model: Contribution
+            ) {
+                holder.notess.setText(model.notes);
+                Picasso.get().load(model.imgurl).into(holder.imgs)
+
+                holder.from.setText(model.timefrom);
+                holder.till.setText(model.timetill);
+
+            }
+
+        }
+        firebaseRecyclerAdapter.startListening();
+        mRecyclerview.adapter=firebaseRecyclerAdapter;
+    }
+
     companion object {
 
         @JvmStatic
@@ -47,3 +118,11 @@ class mycontribution : Fragment() {
             }
     }
 }
+class contributeViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+    var imgs=itemView.findViewById<ImageView>(R.id.idimage);
+    var notess=itemView.findViewById<TextView>(R.id.idnotess);
+    var from=itemView.findViewById<TextView>(R.id.idfrom);
+    var till=itemView.findViewById<TextView>(R.id.idtill);
+
+}
+
